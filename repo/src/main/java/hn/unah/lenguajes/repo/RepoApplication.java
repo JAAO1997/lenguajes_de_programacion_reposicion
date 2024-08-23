99dd6a62c7b68219598e2003c2ac9850eb0b043a
JAAO97 <jeser.amaya@unah.hn>
2024-08-22 20:20:09 -0600

    Prueba2

---
+++ repo/src/main/java/hn/unah/lenguajes/repo/RepoApplication.java
@@ -0,0 +1,13 @@
+package hn.unah.lenguajes.repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoApplication.class, args);
+	}
+
+}
---
+++ repo/src/main/java/hn/unah/lenguajes/repo/modelos/Equipo.java
@@ -0,0 +1,14 @@
+package hn.unah.lenguajes.repo.modelos;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "equipos")
+
+public class Equipo {
+
+   @ 
+    
+}
